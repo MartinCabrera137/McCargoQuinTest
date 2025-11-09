c459434c21bf59519d3cec6661522d7b301f6051
MartinCabrera137 <martincabrerahdz@gmail.com>
2025-11-09 14:28:44 -0600

    "shared colors"

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppCustomColors {
+  //Colores Primarios
+  static const Color primaryBlue = Color(0xFF0D47A1);
+  static const Color accentBlue = Color(0xFF42A5F5);
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+
+  // secundaris
+  static const Color lightBlue = Color(0xFFE3F2FD); // Azul muy claro para fondos
+  static const Color backgroundGrey = Color(0xFFF5F5F5); // Gris muy claro
+  static const Color mediumGrey = Color(0xFF9E9E9E);
+
+  // Colores de Estad
+  static const Color successGreen = Color(0xFF4CAF50);
+  static const Color warningOrange = Color(0xFFFF9800);
+  static const Color errorRed = Color(0xFFF44336);
+}
